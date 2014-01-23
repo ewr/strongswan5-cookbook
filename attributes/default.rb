b7fda27703c9db1858d5ea0e636d10f85e4ad15b
Eric Richardson <e@ericrichardson.com>
2014-01-22 20:11:18 -0500

    0.0.2: Change attributes to use `node.strongswan5`

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
-default.strongswan.shared_secret  = "CHANGEMEINSECURE"
-default.strongswan.user_group     = "vpn"
-default.strongswan.left           = node.ipaddress
-default.strongswan.vpn_id         = "strongswanvpn"
-default.strongswan.subnets        = "10.0.0.0/24"
-default.strongswan.ip_range       = "10.0.2.20/25"
-default.strongswan.rightdns       = "8.8.8.8,8.8.4.4"
+default.strongswan5.shared_secret  = "CHANGEMEINSECURE"
+default.strongswan5.user_group     = "vpn"
+default.strongswan5.left           = node.ipaddress
+default.strongswan5.vpn_id         = "strongswanvpn"
+default.strongswan5.subnets        = "10.0.0.0/24"
+default.strongswan5.ip_range       = "10.0.2.20/25"
+default.strongswan5.rightdns       = "8.8.8.8,8.8.4.4"
